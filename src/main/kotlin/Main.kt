8656e1ca05dcce048fcd1dfd1ed0d660e72cddd1
Sean Proctor <sproctor@gmail.com>
2024-05-02 09:07:58 -0400

    add a real drawer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.launch
 
 @Composable
 @Preview
@@ -17,14 +17,40 @@ fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
         ModalNavigationDrawer(
-            drawerContent = {},
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet {
+                    Text("Drawer Content")
+                }
+            },
 //            gesturesEnabled = false,
         ) {
-            Button(onClick = {
-                text = "Hello, Desktop!"
-            }) {
-                Text(text)
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text(text) },
+                        navigationIcon = {
+                            val scope = rememberCoroutineScope()
+                            IconButton(
+                                onClick = {
+                                    scope.launch {
+                                        drawerState.open()
+                                    }
+                                }
+                            ) {
+                                Icon(Icons.Filled.Menu, contentDescription = null)
+                            }
+                        }
+                    )
+                }
+            ) {
+                Button(onClick = {
+                    text = "Hello, Desktop!"
+                }) {
+                    Text(text)
+                }
             }
         }
     }
