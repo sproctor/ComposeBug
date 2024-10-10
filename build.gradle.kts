92ac7133477ee78a31650a74df6ceda890f09d0f
Sean Proctor <sproctor@gmail.com>
2024-10-10 18:48:03 -0400

    Reproduce conveyor bundled sqlite bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,15 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.compose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.hydraulic.conveyor)
 }
 
 group = "com.example"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
+
+kotlin {
+    jvmToolchain(17)
+}
 
 dependencies {
     // Note, if you develop a library, you should use compose.desktop.common.
@@ -17,6 +22,10 @@ dependencies {
     implementation(compose.desktop.currentOs)
     implementation(compose.material3)
     implementation(libs.sqlite.bundled)
+
+    // Needed for Conveyor
+    "linuxAmd64"(compose.desktop.linux_x64)
+    "windowsAmd64"(compose.desktop.windows_x64)
 }
 
 compose.desktop {
@@ -25,8 +34,16 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "ComposeDragBug"
+            packageName = "ComposeBug"
             packageVersion = "1.0.0"
+        }
+
+        buildTypes {
+            release {
+                proguard {
+                    configurationFiles.from("proguard-rules.pro")
+                }
+            }
         }
     }
 }
@@ -34,3 +51,12 @@ compose.desktop {
 tasks.wrapper {
     gradleVersion = "8.8"
 }
+
+// region Work around temporary Compose bugs.
+configurations.all {
+    attributes {
+        // https://github.com/JetBrains/compose-jb/issues/1404#issuecomment-1146894731
+        attribute(Attribute.of("ui", String::class.java), "awt")
+    }
+}
+// endregion
---
+++ proguard-rules.pro
@@ -0,0 +1,5 @@
+
+-dontwarn kotlinx.**
+-keep class org.jetbrains.skiko.** { *; }
+-keep class androidx.sqlite.driver.bundled.* { *; }
+-keep class androidx.sqlite.** { *; }
