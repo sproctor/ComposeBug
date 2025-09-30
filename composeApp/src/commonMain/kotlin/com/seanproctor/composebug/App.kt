8e012dede7fd2d13bf68bcfc8e6c1e0891e3a8a0
Sean Proctor <sproctor@gmail.com>
2025-09-29 23:22:33 -0400

    add backgroundcolor and text to header

--- composeApp/src/commonMain/kotlin/com/seanproctor/composebug/App.kt
+++ composeApp/src/commonMain/kotlin/com/seanproctor/composebug/App.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.WideNavigationRail
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -36,7 +37,9 @@ fun App() {
             WideNavigationRail(
                 header = {
                     if (showContent) {
-                        Box(modifier = Modifier.height(100.dp))
+                        Box(modifier = Modifier.height(150.dp).background(Color.Red)) {
+                            Text("Header")
+                        }
                     }
                 }
             ) {
