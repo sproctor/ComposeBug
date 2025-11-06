b0b9654c75dfd7e8ea52d875bcf2a9438a99991b
Sean Proctor <sproctor@gmail.com>
2025-11-05 19:24:41 -0500

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,18 @@ dependencyResolutionManagement {
 
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.60.5"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("de.fayard.refreshVersions") version "0.60.6"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "ComposeBug"
+
+refreshVersions {
+    // work-around https://github.com/Splitties/refreshVersions/issues/640
+    file("build/tmp/refreshVersions").mkdirs()
+    versionsPropertiesFile = file("build/tmp/refreshVersions/versions.properties")
+
+    rejectVersionIf {
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
+}
